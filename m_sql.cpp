4c8633efd014acaab9cda320191e78350b1cea14
Alexandr Kovalev <kovalev55@mail.ru>
2018-09-20 16:47:20 +0300

    first commit

---
+++ m_sql.cpp
@@ -0,0 +1,18 @@
+#include "m_sql.h"
+
+// user: Alex
+// psw : admin
+// bd  : bd_Alex
+m_sql::m_sql()
+{
+
+}
+m_sql::try1()
+{
+    QSqlDatabase db = QsqlDatabase::addDatabase("QMYSQL", "mydb");
+    db.setHostName("bigblue");
+    db.setDatabaseName("flightdb");
+    db.setUserName("acarlson");
+    db.setPassword("1uTbSbAs");
+    bool ok = db.open();
+}
---
+++ m_sql.h
@@ -0,0 +1,12 @@
+#ifndef M_SQL_H
+#define M_SQL_H
+#include <QtSql/QSql>
+
+class m_sql
+{
+public:
+    m_sql();
+    void try1();
+};
+
+#endif // M_SQL_H
